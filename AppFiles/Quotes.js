6bdbfb6e8d5ff73089617ff50358bb8c1546685b
Kay Who Codes <68508154+kay-who-codes@users.noreply.github.com>
2026-08-15 11:45:07 +0100

    Fix formatting of QUOTES array in Quotes.js

--- AppFiles/Quotes.js
+++ AppFiles/Quotes.js
@@ -1,5 +1,5 @@
 const QUOTES = [
     "The only way to do great work is to love what you do.",
     "The unexamined life is not worth living.",
-    "The only true wisdom is in knowing you know nothing.",
+    "The only true wisdom is in knowing you know nothing."
 ];
